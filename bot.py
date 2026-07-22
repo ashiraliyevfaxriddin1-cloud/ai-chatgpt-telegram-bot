49707e18b950d9220c94bd4911511fe2e26af341
ashiraliyevfaxriddin1-cloud <ashiraliyevfaxriddin1@gmail.com>
2026-07-23 02:26:09 +0500

    Add Telegram bot functionality with start command

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
+import telebot
+import os
 
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+print("TOKEN BOR:", bool(TOKEN))
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Ishladim 🤖")
+
+bot.infinity_polling()
